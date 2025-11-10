76b9b92c098cf32963023e2ab2efe4a7a420faa7
SaraIslahi <islahi.sarah3@gmail.com>
2025-11-10 08:59:13 +0100

    Add files via upload

---
+++ 01_Python_Fundamentals/Exercises/week1/Day3/exercise_8.py
@@ -0,0 +1,11 @@
+print("exercise 8: what is your name".capitalize())
+# Ask the user for their name 
+#try:
+name=input("what is your name").capitalize()
+  # A funny message
+if name=="Sara":
+    print(f"hello {name} we have the same name. We're practically twins... ")
+else:
+    print(f"A pleasure to meet you, {name}! That's a lovely name. Mine is Sara, but I'm not offended if yours is better.")
+
+
